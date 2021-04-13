444062aeeb0d4c6d0d43305162dbacb97019da8a
yilmale <yilmale@auburn.edu>
2021-04-13 12:16:12 -0500

    FCM update

--- src/SampleModule.jl
+++ src/SampleModule.jl
@@ -1,11 +1,39 @@
 module SampleModule
 
+using PyCall
+using Conda
+
+include("Submodel/PyInterface.jl")
+using .PyInterface
+
+"""
+Some nice documentation here.
+
+# Examples
+```jldoctest
+julia> a = [1 2; 3 4]
+2×2 Array{Int64,2}:
+ 1  2
+ 3  4
+```
+"""
 greet() = print("Hello World!")
 
-include("Submodel/Submodel.jl")
-using .SubModel
 
-greetSub()
-greetSub2()
+function initPyInterface()
+    Conda.pip_interop(true, Conda.ROOTENV)
+    Conda.pip("install", "ema_workbench")
+    Conda.pip("install", "scipy")
+    Conda.pip("install", "altair")
+    Conda.add("ipyparallel")
+    Conda.pip("install", "SALib")
+    Conda.pip("install", "platypus-opt")
+end
+
+#include("Submodel/Submodel.jl")
+#using .SubModel
+
+#greetSub()
+#greetSub2()
 
 end # module
